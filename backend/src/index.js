df6e07c80edc7fcc9a3e89f9195ae0aa3a2e01ca
code-vedant <162135022+code-vedant@users.noreply.github.com>
2025-04-20 09:07:14 +0530

    new regex

--- backend/src/index.js
+++ backend/src/index.js
@@ -34,8 +34,20 @@ app.use("/api/messages", messageRouter);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
+  });
+  app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
+  });
+  app.get("/signup", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
+  });
+  app.get("/profile", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
+  });
+  app.get("/settings", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
   });
 }
 
