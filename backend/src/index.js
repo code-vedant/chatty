130493e2e18bf429e2f796f550dc1e75b6c6a736
code-vedant <162135022+code-vedant@users.noreply.github.com>
2025-07-08 14:25:22 +0530

    deploy updates

--- backend/src/index.js
+++ backend/src/index.js
@@ -23,7 +23,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173","https://chattynew.vercel.app",process.env.VITE_URL],
     credentials: true,
   })
 );
